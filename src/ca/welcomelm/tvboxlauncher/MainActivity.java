13624c043fe295b2133627f3e35b60475101002b
Min Li <mli@blackberry.com>
2014-03-05 17:24:42 -0500

    First day work

--- src/ca/welcomelm/tvboxlauncher/MainActivity.java
+++ src/ca/welcomelm/tvboxlauncher/MainActivity.java
@@ -3,6 +3,10 @@ package ca.welcomelm.tvboxlauncher;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +21,13 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void showPopup(View v) {
+	    PopupMenu popup = new PopupMenu(this, v);
+	    MenuInflater inflater = popup.getMenuInflater();
+	    inflater.inflate(R.menu.main, popup.getMenu());
+	    popup.show();
 	}
 
 }
